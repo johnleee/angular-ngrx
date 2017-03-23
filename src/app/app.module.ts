8720621bcfdd73d0ebb396d82802e8f77dd611d6
Lee <jlee@openlane.com>
2017-03-23 19:47:47 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HomeUiComponent } from './home-ui/home-ui.component';
+import { HomeStoreComponent } from './home-store/home-store.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeUiComponent,
+    HomeStoreComponent
   ],
   imports: [
     BrowserModule,
